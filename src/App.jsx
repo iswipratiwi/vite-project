dcf4f519cd4bcde52f29ac5a6a9ed71dc06104d0
iswipratiwi <iswipratiwi@apps.ipb.ac.id>
2023-05-28 13:29:04 +0700

    add create todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,49 @@
 import { useState } from 'react'
 import './App.css'
+import { nanoid } from 'nanoid'
+
+const DUMMY_TODO = [
+  {
+    id: nanoid(),
+    title: "Belajar React",
+    isComplete: false
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Todos, setTodos] = useState(DUMMY_TODO)
+  const [newTodo, setNewTodo] = useState('')
+
+function addNewTodo(){
+  const updatedTodos = [...Todos]
+  const objTodo = {
+    id: nanoid(),
+    title: newTodo,
+    isCompleted: false
+  }
+
+  updatedTodos.push(objTodo)
+  setTodos(updatedTodos)
+  setNewTodo('')
+}
 
   return (
     <>
-      Hello World
+      <h1>Todo App</h1>
+      <input 
+        type='text' 
+        placeholder='Mau mengerjakan apa?' 
+        value={newTodo} 
+        onChange={event => setNewTodo(event.target.value)}
+      />
+      <button onClick={() => addNewTodo()}>Create</button>
+      <ul>
+        {
+          Todos.map((todo) => (
+            <li key = {todo.id}>{todo.title} </li>
+          ))
+        }
+      </ul>
     </>
   )
 }
